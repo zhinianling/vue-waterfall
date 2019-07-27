906cab81339371a719cdf84cf59d538ee8b05e78
zhinianling <1272709716@qq.com>
2019-07-27 13:33:22 +0800

    瀑布流简单实现

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <vWaterfall></vWaterfall>
+  </div>
+</template>
+
+<script>
+import vWaterfall from './plugin/v-waterfall'
+
+export default {
+  name: 'app',
+  components: {
+    vWaterfall
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
